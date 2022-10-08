507dba281bff65f4f32973dd4126b04134f0d699
Enamul Ali <enamul97@icloud.com>
2022-10-08 17:43:36 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import List from "./components/List";
+import Add from "./components/Add";
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Nav from "./components/Nav";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Header />
+      {window.location.pathname === "/login" ? null : <Nav />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 style={{ padding: 2 }}>Budgetting App</h1>
+    </div>
+  );
+};
+
+export default Header;
