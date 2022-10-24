4d773287aeba82ee675cc6c36cbf8bb9ad4c8bf6
Enamul Ali <enamul97@icloud.com>
2022-10-24 17:07:23 +0100

    add login form styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,34 +25,47 @@ const Login = () => {
   };
 
   return (
-    <>
-      <form>
-        <div className="email">
-          <p>email</p>
-          <input
-            type="email"
-            onChange={(e) => {
-              setLoginEmail(e.target.value);
-            }}
+    <div className="px-6 h-full text-gray-800">
+      <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+        <div class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            class="w-full"
+            alt="Sample image"
           />
         </div>
-        <div className="email">
-          <p>password</p>
-          <input
-            type="password"
-            onChange={(e) => {
-              setLoginPassword(e.target.value);
-            }}
-          />
+        <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+          <form>
+            <div className="mb-6">
+              <p>email</p>
+              <input
+                type="email"
+                onChange={(e) => {
+                  setLoginEmail(e.target.value);
+                }}
+                className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              />
+            </div>
+            <div className="mb-6">
+              <p>password</p>
+              <input
+                type="password"
+                onChange={(e) => {
+                  setLoginPassword(e.target.value);
+                }}
+                class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              />
+            </div>
+            <button
+              onClick={handleLogin}
+              className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Login
+            </button>
+          </form>
         </div>
-        <button
-          onClick={handleLogin}
-          className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
-        >
-          Login
-        </button>
-      </form>
-    </>
+      </div>
+    </div>
   );
 };
 
